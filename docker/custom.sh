e6e3fb52c769e3f4c402e8002d6232fbfcf9093a
Raphael van Kempen <vankempen@thinking-cars.de>
2026-08-29 09:56:12 +0200

    Revert "install autonomy_dataset_msgs only"
    This reverts commit c72d8affa73b82a52c7fd6e0b1523ab837717175.

--- docker/custom.sh
+++
@@ -1,4 +0,0 @@
-# only needed for the autonomy_datasets_msgs package, which carries the dataset
-# meta information that perception_msgs/Object cannot express
-git clone --branch v1.5.0 https://github.com/thinking-cars/autonomy_datasets.git src/upstream/autonomy_datasets
-rm -r src/upstream/autonomy_datasets/autonomy_datasets/
